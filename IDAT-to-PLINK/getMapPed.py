4d5ef6c1aa533c9f44404a41e800cacfb413761e
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2025-07-29 14:44:25 -0500

    Update getMapPed.py

--- IDAT-to-PLINK/getMapPed.py
+++ IDAT-to-PLINK/getMapPed.py
@@ -2,15 +2,16 @@ import pandas as pd
 
 def idat_to_map(df: pd.DataFrame, map_file_name: str) -> None:
     '''
-    df: a merged data frame containing the chromosome number, rsid, 
-    base position, and genotypes for each individual. If only the rsID 
-    is available, please refer to IDAT-to-PLINK/getLocation.R 
+    df: a merged data frame containing each SNP with its corresponding 
+    chromosome number and position 
     map_file_name: the output file name. Should have the .map extension. 
     '''
-    df = df.sort_values(by=["Chr", "Position"])
     df = df.iloc[:, :3]
-    df["new"] = 0 # corresponds to centimorgan length (0 is default) 
+    df = df.sort_values(by=[df.columns[1], df.columns[2]])
+    df["new"] = 0 
     # reorder in .map format https://www.cog-genomics.org/plink/1.9/formats#map
-    cols = ["chr_name", "refsnp_id", "new", "chrom_start"]
+    cols = [df.iloc[:, 1].name, df.iloc[:, 0].name, "new", df.iloc[:, 2].name]
     to_map = df.reindex(columns=cols)
     to_map.to_csv(map_file_name, sep = " ", index=False, header=False)
+
+def idat_to_ped(): 

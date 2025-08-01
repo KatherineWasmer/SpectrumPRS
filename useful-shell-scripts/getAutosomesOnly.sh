9b1f62b3719a33bb3de001f72e967330b737a92a
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2025-08-01 00:30:43 -0500

    Update getAutosomesOnly.sh

--- useful-shell-scripts/getAutosomesOnly.sh
+++ useful-shell-scripts/getAutosomesOnly.sh
@@ -3,9 +3,11 @@
 
 module load Bioinformatics 
 module load bcftools 
+pfx = "sorted" 
+at = "at" # autosomal 
 # sort vcf 
-bcftools sort A102092.vcf.gz -Oz -o A102092_sorted.vcf.gz
+bcftools sort $1 -Oz -o {$pfx}{$1}
 # index sorted vcf 
-tabix -p vcf A102092_sorted.vcf.gz
+tabix -p vcf {$pfx}{$1}
 # filter autosomes (no spaces between commas) 
-bcftools view -r chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22 -Oz -o A102092_sorted_at.vcf.gz A102092_sorted.vcf.gz
+bcftools view -r chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22 -Oz -o {$at}{$pfx}{$1}

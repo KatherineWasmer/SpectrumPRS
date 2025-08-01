d087cf25bad2854843f79ff1a7a4bb6cedb00351
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2025-07-31 19:15:07 -0500

    Update plinkToVCF.sh
    Input format: ./plinkToVCF.sh filePrefix

--- useful-shell-scripts/plinkToVCF.sh
+++ useful-shell-scripts/plinkToVCF.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 module load Bioinformatics 
-module load plink/1.9 
-echo Please enter the prefix of your BED/BIM/FAM files. These should all be the same. 
-read myFile   
-plink --bfile $myFile --recode-vcf --out $myFile  
+module load plink/1.9   
+plink --bfile $1 --recode-vcf --out $1  

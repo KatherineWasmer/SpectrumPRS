dbd341310fc50990cf3af5d351e7026fdbca2c73
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2025-08-01 00:15:13 -0500

    Update getAutosomesOnly.sh

--- useful-shell-scripts/getAutosomesOnly.sh
+++ useful-shell-scripts/getAutosomesOnly.sh
@@ -3,4 +3,9 @@
 
 module load Bioinformatics 
 module load bcftools 
+# sort vcf 
+./sortVCF.sh 
+# index sorted vcf 
+./indexVCF.sh
+# filter autosomes (no spaces between commas) 
 bcftools view -r chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22 -Oz -o A102092_sorted_at.vcf.gz A102092_sorted.vcf.gz

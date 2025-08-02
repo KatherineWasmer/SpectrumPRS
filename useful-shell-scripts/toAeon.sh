43ed106204c864bbe654bdcae3e1fbec10e2d0e3
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2025-08-02 13:59:04 -0500

    Update toAeon.sh

--- useful-shell-scripts/toAeon.sh
+++ useful-shell-scripts/toAeon.sh
@@ -1,13 +1,18 @@
 
 input_file = $1
-if [[ $input_file == "*.gz"]]; then 
-  base_name=$(basename "$input_file" .vcf.gz)
-else 
-  base_name=$(basename "$input_file" .vcf)
+base_name=$(basename "$input_file" .vcf.gz)
+
+annot_file = ${base_name}${"chr.vcf.gz"}
+
+ancestry_output = ${annot_file}${"ancestry"}
+
+# create file for renaming chromosomes in aeon annotation 
+if [ ! -f rename_chr.txt ] then; 
+  for i in {1..22}; do echo -e "$i\tchr$i"; done > rename_chr.txt
 
 # Step 1: annotate 
-bcftools annotate --rename-chrs rename_chr.txt -Oz -o japan_autism_v2_chr.qc.vcf japan_autism_v2.qc.vcf
+bcftools annotate --rename-chrs rename_chr.txt -Oz -o annot_file input_file
 # Step 2: index 
-bcftools index --csi 0_GSM4300828_chr.vcf.gz
+bcftools index --csi annot_file
 # Step 3: compute ancestry 
-aeon 0_GSM4300828.vcf.gz -o 0_GSM4300828_ancestry --v
+aeon annot_file -o ancestry_output --v

9bb0e3aa4be65b9782524009fd5e7de8dd7d9878
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2025-08-02 11:01:14 -0500

    Update getAutosomesOnly.sh

--- useful-shell-scripts/getAutosomesOnly.sh
+++ useful-shell-scripts/getAutosomesOnly.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 module load Bioinformatics 
 module load bcftools 
+module load samtools 
+module load htslib 
 
-input_file = $1
+input_file=$1
 base_name=$(basename "$input_file" .vcf.gz)
-output_file = "${base_name}_at.vcf.gz"
+output_file="${base_name}_at.vcf.gz"
 # filter autosomes (no spaces between commas) 
-bcftools view -r chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22 -Oz -o "$output_file" "$input_file" 
+bcftools view -r 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22 -Oz -o "$output_file" "$input_file"
+tabix -p vcf "$output_file"

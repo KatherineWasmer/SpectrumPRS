0536447bc5f4861ff656d7e79ced61fe4d3ef717
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2025-08-07 01:39:23 -0500

    Update getAncestry.sh

--- useful-shell-scripts/getAncestry.sh
+++ useful-shell-scripts/getAncestry.sh
@@ -1,8 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=ancestry_LaSalle
+#SBATCH --output=ancestry_%A_%a.out
+#SBATCH --error=ancestry_%A_%a.err
+#SBATCH --array=0-9
+#SBATCH --time=2:00:00
+#SBATCH --mem=8G
 
+cd ~/Capstone
+
+# Get sorted files for this task in GrCh37 format 
+file=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" sorted_files.txt)
+echo "Processing $file..."
+
+base=$(basename "$file" .vcf.gz)
 
 # rename chromosomes for AEon format 
-bcftools annotate --rename-chrs rename_chr.txt -Oz -o A106109_cleaned_chr.vcf.gz A106109_cleaned.vcf.gz
+renamed_chr_file = "GSE178204_RAW/${base}_chr.vcf.gz"
+bcftools annotate --rename-chrs rename_chr.txt -Oz -o $renamed_chr_file $file
+
 # intersect allele frequencies with given SNPs 
-bedtools intersect -a g1k_allele_freqs.txt -b A106109_cleaned_chr.vcf.gz -header > intersected_AFs.txt
+intersected_file = "${base}_intersectedAFs.txt"
+bedtools intersect -a g1k_allele_freqs.txt -b A106109_cleaned_chr.vcf.gz -header > $intersected_file
+
 # run ancestry 
-aeon A106109_cleaned_chr.vcf.gz -o A106109 -a intersected_AFs.txt
+aeon $renamed_chr_file -o $base -a $intersected_file

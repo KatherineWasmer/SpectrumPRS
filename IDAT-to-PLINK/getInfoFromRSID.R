1a3751064589775f0bf1a5ea9aa51aa21f0c6818
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2025-11-03 14:01:44 -0600

    Enhance getInfoFromRSID with build and species parameters
    Updated getInfoFromRSID function to include parameters for build and species.

--- IDAT-to-PLINK/getInfoFromRSID.R
+++ IDAT-to-PLINK/getInfoFromRSID.R
@@ -1,8 +1,16 @@
 BiocManager::install("biomaRt")
 library(biomaRt) 
 
-getInfoFromRSID <- function(rsidFile, outputFile){
-  snp_mart <- useMart(biomart="ENSEMBL_MART_SNP", host="https://grch37.ensembl.org", path = "/biomart/martservice", dataset="hsapiens_snp")
+# Function objective: Retrieves chromosome numbers, major/minor alleles, and base pair locations based off of an rsID. 
+# Default paramenters - Human Genome Reference Build 37
+getInfoFromRSID <- function(rsidFile, outputFile, build, species){
+  if (missing(build)) {
+    build = "https://grch37.ensembl.org"
+  }
+  if (missing(species)) {
+    species = "hsapiens_snp"
+  }
+  snp_mart <- useMart(biomart="ENSEMBL_MART_SNP", host=build, path = "/biomart/martservice", dataset=species)
   rsids <- read.csv("rsids.txt", sep = "\t")
   new_df <- getBM(attributes = c('refsnp_id', 'chr_name', 'chrom_start', 'allele'), 
              filters = c('snp_filter'), 

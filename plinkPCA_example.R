7a45e5f721ed4fe2943d8bf1de1a94e5c9adab78
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2025-12-04 15:58:01 -0600

    Add optional plink installation steps
    Added optional installation steps for plink in the script.

--- plinkPCA_example.R
+++ plinkPCA_example.R
@@ -2,6 +2,10 @@
 # Upload this file to your Galaxy account and rename it chr3.vcf.gz and create a session in R 
 # Add this file to the galaxy_inputs folder in your R session 
 
+# OPTIONAL: install plink 
+system("wget -P ~/bin/ https://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20231211.zip")
+system("unzip ~/bin/plink_linux_x86_64_20231211.zip -d ~/bin/")
+
 system("~/plink --vcf galaxy_inputs/chr3.vcf.gz --make-bed --out chr3") 
 system("~/plink --bfile chr3 --keep-allele-order --pca 20 --out chr3PCA") # outputs a file called chr3PCA.eigenvec 
 

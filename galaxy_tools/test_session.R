a96f4bdd2a692577a477245e19e5a135e49a786a
Kate <145235561+KatherineWasmer@users.noreply.github.com>
2026-02-16 13:13:05 -0600

    Create test_session.R

---
+++ galaxy_tools/test_session.R
@@ -0,0 +1,12 @@
+# running Glados functions within Galaxy tools and creating a PCA with the human genome project samples 
+devtools::install_from_github("KatherineWasmer/glados") 
+library(glados)
+
+glados::galaxy_install_plink()
+glados::galaxy_rename_file("1_chr1_1KG.vcf.gz.vcf_bgzip", "chr1.vcf.gz")
+glados::galaxy_create_bed("chr1.vcf.gz", "chr1")
+
+# still need to add to library
+get_pca <- function(filename, n_components, output_name){
+  system(glue::glue("~/bin/plink --bfile {filename} --keep-allele-order --pca {n_components} --out {output_name}"))
+}
